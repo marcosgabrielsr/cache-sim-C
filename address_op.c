eff2f6c99373ef005b6c67f0107f08a016c77379
marcosgabrielsr <marcosdevse@gmail.com>
2025-04-23 14:42:06 -0400

    docs: update comments

--- address_op.c
+++ address_op.c
@@ -1,12 +1,12 @@
 // Including libs
 #include "cache.h"
 
-// Return the index in the cache of the address variable
+// Return the index in the cache of the address variable (address mod (m/n))
 unsigned int get_index(int address, int x) {
     return address % x;
 }
 
-// Return the tag of the address
+// Return the tag of the address (address / (m/n))
 unsigned int get_tag(int address, int x) {
     return address / x;
 }
